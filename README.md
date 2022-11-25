9450e19f3cc3493910387abb109b3352dadc268b
Moses-Adeyemi <92913316+Moses-Adeyemi@users.noreply.github.com>
2022-11-25 19:59:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# grid-react-Table
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ag-grid-react-hello-world-j9vvfw)
---
+++ index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const App = () => {
+  const [rowData] = useState([
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+  ]);
+
+  const [columnDefs] = useState([
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+  ]);
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
